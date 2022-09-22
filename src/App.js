279c75c978c31046fa60c071cffba40a5efbc98b
michaels90up <michael1990spw@gmail.com>
2022-09-22 16:05:50 +0200

    Adding button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 export default function App() {
-  return <div>a</div>;
+  return (
+    <div>
+      <form>
+        <label>
+          First name:
+          <input name="first name" />
+        </label>
+      </form>
+      <form>
+        <label>
+          Last name:
+          <input name="last name" />
+        </label>
+      </form>
+      <button className="delete button">Remove</button>
+    </div>
+  );
 }
